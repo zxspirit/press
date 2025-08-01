17fb0f5a04a9b854e29a0a9877ab3f3907e7e154
zhxu <zheng2580369@gmail.com>
2025-08-01 16:46:43 +0800

    init

---
+++ src/main/kotlin/com/newzhxu/press/PressApplication.kt
@@ -0,0 +1,11 @@
+package com.newzhxu.press
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class PressApplication
+
+fun main(args: Array<String>) {
+    runApplication<PressApplication>(*args)
+}
---
+++ src/test/kotlin/com/newzhxu/press/PressApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.newzhxu.press
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class PressApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
